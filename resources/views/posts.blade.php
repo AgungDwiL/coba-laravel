f8c3c9ab8fe95d7071150eaf69bef7bc1460459c
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-11-22 09:01:20 +0700

    init project
    inisialisasi pengulangan materi belajar laravel menggunakan panduan tutorial laravel 8 sandhika galih

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>Agung's Blog | Blog</title>
+</head>
+<body>
+    Halaman Blog
+    <h3>Agung Dwi Laksana</h3>
+    <p>agung.laksana16@gmail.com</p>
+
+    <img src="img/agung" alt="foto_agung" width="200px">
+    
+</body>
+
+    <script src="js/script.js"></script>
+</html>
