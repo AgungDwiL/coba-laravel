0a02d2ae1d373fa6560237555e52a23c6323bbd7
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-11-29 12:44:45 +0700

    memperbaiki tampilan categories
    memperbagus UI

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -4,15 +4,22 @@
 
 @section('body')
 <div class="container mt-3">
-  <h2>Post's Category</h2>
-    @foreach ($categories as $category)
-        <ul>
-          <li>
-            <a href="{{ url('category/' . $category->slug) }}">  
-                <h4>{{ $category->name }}</h4>
-            </a>
-          </li>
-        </ul>
-    @endforeach
+  <h1 class="mb-5">Post's Category</h1>
+  <div class="container">
+    <div class="row">
+      @foreach ($categories as $category)
+        <div class="col-md-4 p-2">
+          <a href="{{ url('category/' . $category->slug) }}">
+            <div class="card bg-dark text-white">
+              <img class="card-img" src="https://www.sourcesplash.com/i/random?q={{ $category->name }}&w=500&h=300" alt="{{ $category->name }}">
+              <div class="card-img-overlay d-flex align-items-center p-0">
+                <h4 class="card-title text-center flex-fill p-3 fs-3" style="background-color: rgba(0,0,0,0.7);">{{ $category->name }}</h4>
+              </div>
+            </div>
+          </a>
+        </div>  
+      @endforeach
+    </div>
+  </div>
 </div>
 @endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,13 +3,25 @@
 @section('title', 'Blog: ' . $post->title)
 
 @section('body')
-<div class="container mt-3">
-    <article>
-      <h3>{{ $post->title }}</h3>
-      <h6>by: <a href="{{ url('author/' . $post->author->username) }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="{{ url('category/' . $post->category->slug) }}">{{ $post->category->name }}</a></h6>
-      {!! $post->body !!}
-    </article>
+
+<div class="container">
+  <div class="row justify-content-center mb-5">
+    <div class="col-md-8">
+      <h1 class="mb-3">{{ $post->title }}</h1>
+      <p>by: <a href="{{ url('author/' . $post->author->username) }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="{{ url('category/' . $post->category->slug) }}">{{ $post->category->name }}</a></p>
+
+      <img src="https://www.sourcesplash.com/i/random?q={{ $post->category->name }}&w=1200&h=400" alt="{{ $post->category->name }}" class="img-fluid">
+
+      <article class="my-3 fs-5">
+        {!! $post->body !!}
+      </article>
     
-    <a href="{{ url('posts') }}" class="btn btn-secondary mt-4">Back</a>  
+      <a href="{{ url('posts') }}" class="btn btn-secondary mt-4">Back</a>
+    </div>
+  </div>
+</div>
+
+<div class="container mt-3">
+      
 </div>
 @endsection
