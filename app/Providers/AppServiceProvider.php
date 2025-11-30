844d24b2583ef022de18ad6e8b498f4b0cd65c37
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-11-30 22:03:17 +0700

    membuat otorisasi dengan gate
    basic authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
+use App\User;
 use Illuminate\Support\ServiceProvider;
-use Clockwork\Support\Laravel\Clockwork as ClockworkSupport;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // 
+        Gate::define('admin', function(User $user){
+            return $user->username === 'agungdwilaksana';
+        });
     }
 }
